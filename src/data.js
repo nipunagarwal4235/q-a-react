c667e6ddce298159fca0f33eb65a4595c4fdd08f
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-26 03:16:56 +0530

    deployed..

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is my name?',
+    info:
+      'My name is Nipun Agarwal. Out of all the agarwal spellings out there , my family chose "agarwal". Nipun means "expert". TBH I still dont know what I am expert at.',
+  },
+  {
+    id: 2,
+    title: 'Where am I studying right now?',
+    info:
+      'I am studying at BML Munjal University located in Gurgaon , Haryana. I will pass out in 2024. Hopefully with some offline campus life experience.',
+  },
+  {
+    id: 3,
+    title: 'What are your hobbies?',
+    info:
+      'I dont have any mainstream hobbies like singing or dancing. I enjoy travelling and eating junk food a lot. I like to meet new people and socialise. ',
+  },
+  {
+    id: 4,
+    title: 'What are you currently pursuing and your future plans?',
+    info:
+      'I am currently pursuing Btech in Computer Science. No such future plans as of now. I like to go with the flow. I know I know only dead fish go with the flow but what if the fish manages to stay alive?',
+  },
+  {
+    id: 5,
+    title: 'Items in your bucket list ?',
+    info:
+      'Anything very very spontaneous with an adrenline rush is automatically added onto my bucket list.',
+  },
+]
+export default questions
