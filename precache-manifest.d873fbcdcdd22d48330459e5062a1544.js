1cd2f6a0430f1c4f2f4cc9947a66d10a6de57552
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-26 03:18:14 +0530

    Updates

---
+++ precache-manifest.d873fbcdcdd22d48330459e5062a1544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627688824998bab6540f05665c0641ea",
+    "url": "/q-a-react/index.html"
+  },
+  {
+    "revision": "cc595c73d97a35dfd0e5",
+    "url": "/q-a-react/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/q-a-react/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/q-a-react/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc595c73d97a35dfd0e5",
+    "url": "/q-a-react/static/js/main.ddfde350.chunk.js"
+  },
+  {
+    "revision": "832114f991b0809de0e1",
+    "url": "/q-a-react/static/js/runtime-main.cecf32c7.js"
+  }
+]);
